0ff38e3564db8c0776ed03ae26b7aa05323607ba
justinmk21 <kgomotsomkhawane2001@gmail.com>
2025-01-03 03:01:11 +0200

    Update IconButton.jsx
    Changed the # symbol to the Portfolio name

--- src/IconButton.jsx
+++ src/IconButton.jsx
@@ -4,7 +4,7 @@ function IconButton() {
 
     return (
         <div className="iconbutton">
-            <a href='/#'>
+            <a href='/Portfolio'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" width="63px">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M15 11.25l-3-3m0 0l-3 3m3-3v7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
